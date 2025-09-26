ed2e5445122cdf20b2e32d934688c2d09eb1ce82
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2025-09-26 21:57:17 +0200

    Changed quotes to internal system instead of API (because of rate limiting)

--- src/main/kotlin/commands/SimpleUserCommands.kt
+++ src/main/kotlin/commands/SimpleUserCommands.kt
@@ -1,15 +1,7 @@
 package me.m64diamondstar.commands
 
 import dev.minn.jda.ktx.events.listener
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
+import me.m64diamondstar.quotes
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.components.actionrow.ActionRow
 import net.dv8tion.jda.api.components.buttons.Button
@@ -34,11 +26,14 @@ class SimpleUserCommands(jda: JDA) {
         }
     }
 
-    suspend fun handleQuote(event: SlashCommandInteractionEvent) {
+    fun handleQuote(event: SlashCommandInteractionEvent) {
         event.deferReply().queue()
 
         // Fetch random quote from ZenQuotes API
-        event.hook.sendMessage(QuoteRetrieval.fetch())
+        val quote = quotes.random()
+        event.hook.sendMessage(
+            "“${quote.text}” — **${quote.author}**\n" +
+                    "-# _Check out the [source](<https://github.com/dwyl/quotes/blob/main/quotes.json>) of the quotes!_")
             .addComponents(
                 ActionRow.of(
                     Button.secondary("quote-regenerate", "Regenerate")
@@ -49,10 +44,11 @@ class SimpleUserCommands(jda: JDA) {
 
     fun handleRefreshQuote(event: ButtonInteractionEvent) {
         if(event.componentId != "quote-regenerate") return
-        CoroutineScope(Dispatchers.IO).launch {
-            event.editMessage(QuoteRetrieval.fetch())
-                .queue()
-        }
+        val quote = quotes.random()
+        event.editMessage(
+            "“${quote.text}” — **${quote.author}**\n" +
+                    "-# _Check out the [source](<https://github.com/dwyl/quotes/blob/main/quotes.json>) of the quotes!_"
+        ).queue()
     }
 
     fun handleDiscord(event: SlashCommandInteractionEvent) {
@@ -68,40 +64,4 @@ class SimpleUserCommands(jda: JDA) {
         event.reply("You can download EffectMaster here:\n" +
                 "https://modrinth.com/mod/effectmaster/versions").queue()
     }
-
-    private object QuoteRetrieval {
-        private val client = HttpClient(CIO)
-        private val json = Json { ignoreUnknownKeys = true }
-
-        @Serializable
-        data class QuoteResponse(
-            val q: String,
-            val a: String
-        )
-
-        suspend fun fetch(): String {
-            client.get("https://zenquotes.io/api/random").also { response ->
-                if (response.status.value in 200..299) {
-                    val bodyText = response.bodyAsText()
-                    try {
-                        val quotes = json.decodeFromString<List<QuoteResponse>>(bodyText)
-                        val quote = quotes.firstOrNull()
-                        if (quote != null) {
-                            val message =
-                                "“${quote.q}” — **${quote.a}**\n" +
-                                        "-# _Quote provided by [ZenQuotes.io](<https://zenquotes.io>)_"
-                            return message
-                        } else {
-                            return "Couldn’t fetch a quote right now, sorry!"
-                        }
-                    } catch (e: Exception) {
-                        return "Error parsing quote response: ${e.message}"
-                    }
-                } else {
-                    return "Failed to fetch quote: HTTP ${response.status.value}"
-                }
-            }
-        }
-    }
-
 }
